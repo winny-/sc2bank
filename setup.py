07a2ed739f518614f9935d5e947374ca2cec3fb4
Winston Weinert <winston@ml1.net>
2014-02-04 15:11:02 -0600

    Typoes!

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='sc2bank',
       version='0.1',
       description='Validate SC2Bank XML document signatures.',
-      url='https://githubl.com/winny-/sc2bank',
+      url='https://github.com/winny-/sc2bank',
       author='Winston Weinert',
       author_email='sc2bank@fastmail.fm',
       license='MIT / BSD 3-clause',
