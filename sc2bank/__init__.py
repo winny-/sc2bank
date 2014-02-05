a415fc59f3cd34968aa12c4198446e305a084bdf
Winston Weinert <winston@ml1.net>
2014-02-05 12:15:21 -0600

    Forgot to remove __main__ entry point from __init__.py, formally sc2bank.py

--- sc2bank/__init__.py
+++ sc2bank/__init__.py
@@ -168,27 +168,3 @@ def sign(author_id, user_id, name, bank):
         for key in sorted(section.keys):
             update([key.name, 'Value', key.type, key.value])
     return h.hexdigest().upper()
-
-
-if __name__ == '__main__':
-    import sys
-    def usage(v):
-        print('Usage: {0} path/to/Starcraft/BankFile.SC2Bank'.format(sys.argv[0]))
-        sys.exit(v)
-
-    if len(sys.argv) != 2 or sys.argv[1].startswith('-'):
-        usage(0)
-
-    fname = sys.argv[1]
-    if not os.path.isfile(fname):
-        print('"{0}" is not a file.'.format(fname))
-        usage(1)
-
-    signature, recorded_signature = sign_file(fname)
-    print('Calculated signature: {0}'.format(signature))
-    if recorded_signature is None:
-        recorded_signature = '(No signature in XML document.)'
-    print('Recorded signature:   {0}'.format(recorded_signature))
-    if signature != recorded_signature:
-        print('Signatures are NOT equal!')
-        sys.exit(1)
